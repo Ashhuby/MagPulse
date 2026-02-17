1d39b6307a729d92bd5bd3d91f8f646528ed9b6b
Marwan Ashhuby <mashhuby@gmail.com>
2026-02-17 14:06:16 +0000

    Rename project from MagPulse to BounceLab

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,17 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+#include <iostream>  
+#include <vector>
+#include <cmath>
+#include <optional>
 
 int main() {
+    std::cout << "Circle drawn.";
+
     sf::RenderWindow window(sf::VideoMode({800, 600}), "MagPulse SFML 3.0 Test");
+    
     sf::CircleShape shape(50.f);
-    shape.setFillColor(sf::Color::Green);
+    shape.setFillColor(sf::Color::Red);
     shape.setPosition({375.f, 275.f});
 
     while (window.isOpen()) {
